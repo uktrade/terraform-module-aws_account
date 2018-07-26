81d15f47cee268aea10f56ad0b678f747f7c8daf
Bowen Sun <bowensun@gmail.com>
2018-07-26 11:51:11 +0100

    remove member account provider as it won't be available before account created.

--- new-org-member/main.tf
+++ new-org-member/main.tf
@@ -12,28 +12,12 @@ provider "aws" {
   alias = "master"
 }
 
-provider "aws" {
-  alias = "member"
-}
-
 data "aws_region" "master" {
   provider = "aws.master"
 }
 
-data "aws_region" "member" {
-  provider = "aws.member"
-}
-
-data "aws_region" "all_regions" {
-  provider = "aws.member"
-}
-
 data "aws_caller_identity" "master" {
   provider = "aws.master"
-}
-
-data "aws_caller_identity" "member" {
-  provider = "aws.member"
 }
 
 variable "aws_regions" {
