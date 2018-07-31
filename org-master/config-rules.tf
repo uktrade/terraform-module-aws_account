1be7461afc04799a23bda09758dfce8ec7f610ec
Bowen Sun <bowensun@gmail.com>
2018-07-31 11:59:05 +0100

    default AWS Config rules

---
+++ org-master/config-rules.tf
@@ -0,0 +1,118 @@
+resource "aws_config_config_rule" "config_rule_cloudtrail" {
+  provider = "aws.master.config"
+  name = "cloudtrail-enabled"
+  source {
+    owner = "AWS"
+    source_identifier = "CLOUD_TRAIL_ENABLED"
+  }
+  maximum_execution_frequency = "TwentyFour_Hours"
+}
+
+resource "aws_config_config_rule" "config_rule_ec2_instance" {
+  provider = "aws.master.config"
+  name = "ec2-instances-in-vpc"
+  source {
+    owner = "AWS"
+    source_identifier = "INSTANCES_IN_VPC"
+  }
+}
+
+resource "aws_config_config_rule" "config_rule_lambda_public_prohibited" {
+  provider = "aws.master.config"
+  name = "lambda-fucntion-public-access-prohibited"
+  source {
+    owner = "AWS"
+    source_identifier = "LAMBDA_FUNCTION_PUBLIC_ACCESS_PROHIBITED"
+  }
+}
+
+resource "aws_config_config_rule" "config_rule_db_backup_enabled" {
+  provider = "aws.master.config"
+  name = "db-instance-backup-enabled"
+  source {
+    owner = "AWS"
+    source_identifier = "DB_INSTANCE_BACKUP_ENABLED"
+  }
+}
+
+resource "aws_config_config_rule" "config_rule_root_mfa" {
+  provider = "aws.master.config"
+  name = "root-account-mfa-enabled"
+  source {
+    owner = "AWS"
+    source_identifier = "ROOT_ACCOUNT_MFA_ENABLED"
+  }
+  maximum_execution_frequency = "TwentyFour_Hours"
+}
+
+resource "aws_config_config_rule" "config_rule_s3_public_read_prohibit" {
+  provider = "aws.master.config"
+  name = "s3-bucket-public-read-prohibited"
+  source {
+    owner = "AWS"
+    source_identifier = "S3_BUCKET_PUBLIC_READ_PROHIBITED"
+  }
+}
+
+resource "aws_config_config_rule" "config_rule_s3_public_write_prohibit" {
+  provider = "aws.master.config"
+  name = "s3-bucket-public-write-prohibited"
+  source {
+    owner = "AWS"
+    source_identifier = "S3_BUCKET_PUBLIC_WRITE_PROHIBITED"
+  }
+}
+
+resource "aws_config_config_rule" "config_rule_s3_sse" {
+  provider = "aws.master.config"
+  name = "s3-bucket-server-side-encryption-enabled"
+  source {
+    owner = "AWS"
+    source_identifier = "S3_BUCKET_SERVER_SIDE_ENCRYPTION_ENABLED"
+  }
+}
+
+resource "aws_config_config_rule" "config_rule_s3_ssl" {
+  provider = "aws.master.config"
+  name = "s3-bucket-ssl-requests-only"
+  source {
+    owner = "AWS"
+    source_identifier = "S3_BUCKET_SSL_REQUESTS_ONLY"
+  }
+}
+
+resource "aws_config_config_rule" "config_rule_acm_expiration" {
+  provider = "aws.master.config"
+  name = "acm-certificate-expiration-check"
+  source {
+    owner = "AWS"
+    source_identifier = "ACM_CERTIFICATE_EXPIRATION_CHECK"
+  }
+  input_parameters = <<INPUT
+    {
+      "daysToExpiration": "14"
+    }
+  INPUT
+  maximum_execution_frequency = "TwentyFour_Hours"
+}
+
+resource "aws_config_config_rule" "config_rule_iam_password_policy" {
+  provider = "aws.master.config"
+  name = "iam-password-policy"
+  source {
+    owner = "AWS"
+    source_identifier = "IAM_PASSWORD_POLICY"
+  }
+  input_parameters = <<INPUT
+    {
+      "RequireUppercaseCharacters": "true",
+      "RequireLowercaseCharacters": "true",
+      "RequireSymbols": "true",
+      "RequireNumbers": "true",
+      "MinimumPasswordLength": "14",
+      "PasswordReusePrevention": "24",
+      "MaxPasswordAge": "90"
+    }
+  INPUT
+  maximum_execution_frequency = "TwentyFour_Hours"
+}
