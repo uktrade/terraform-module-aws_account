7cd6b28f3839efd1ffc8408154488b4774d46ae8
Bowen Sun <bowensun@gmail.com>
2018-08-07 17:57:42 +0100

    cleanup duplicate resources

--- org-member/cloudwatch.tf
+++ org-member/cloudwatch.tf
@@ -4,12 +4,6 @@ resource "aws_cloudwatch_event_permission" "master" {
   statement_id = "account-${data.aws_caller_identity.member.account_id}"
 }
 
-resource "aws_cloudwatch_event_permission" "master-config" {
-  provider = "aws.master"
-  principal = "${data.aws_caller_identity.member.account_id}"
-  statement_id = "account-${data.aws_caller_identity.member.account_id}"
-}
-
 resource "aws_cloudwatch_event_permission" "master-config_alt" {
   provider = "aws.master.config"
   principal = "${data.aws_caller_identity.member.account_id}"
@@ -24,7 +18,7 @@ resource "aws_cloudwatch_event_permission" "master-config_acm" {
 
 resource "aws_cloudwatch_event_rule" "member" {
   provider = "aws.member"
-  name = "rule-${data.aws_caller_identity.member.account_id}"
+  name = "org-rule-member-${data.aws_caller_identity.member.account_id}"
   event_pattern = "{\"account\": [\"${data.aws_caller_identity.member.account_id}\"]}"
 }
 
@@ -32,7 +26,6 @@ resource "aws_cloudwatch_event_target" "member" {
   provider = "aws.member"
   arn = "${var.org["cloudwatch_eventbus_arn"]}"
   rule = "${aws_cloudwatch_event_rule.member.name}"
-  target_id = "org-member-${data.aws_caller_identity.member.account_id}"
 }
 
 resource "aws_cloudwatch_event_target" "config" {
