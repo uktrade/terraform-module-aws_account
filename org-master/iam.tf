bd3a24cd61d433e99198e5df18c758a63728c22a
Bowen Sun <bowensun@gmail.com>
2018-09-04 17:37:31 +0100

    update policies

--- org-master/iam.tf
+++ org-master/iam.tf
@@ -3,3 +3,23 @@ resource "aws_iam_policy" "default_policy" {
   name = "dit-default"
   policy = "${file("${path.module}/policies/default-policy.json")}"
 }
+
+resource "aws_iam_policy" "default_admin" {
+  provider = "aws.master"
+  name = "dit-default-admin"
+  policy = "${data.aws_iam_policy_document.default_admin.json}"
+}
+
+data "aws_iam_policy_document" "default_admin" {
+  provider = "aws.master"
+  statement {
+    sid = "EnableIAMforAdmin"
+    actions = ["iam:*"]
+    resources = ["*"]
+    condition {
+      test = "StringEquals"
+      variable = "iam:PermissionsBoundary"
+      values = ["${aws_iam_policy.default_policy.arn}"]
+    }
+  }
+}
