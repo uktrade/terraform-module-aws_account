2f8f5e115eb858c24323cc369044c3e73024d716
Bowen Sun <bowensun@gmail.com>
2019-07-05 13:00:45 +0100

    remove optional config

--- org-master/inspector.tf
+++ org-master/inspector.tf
@@ -2,15 +2,9 @@ data "aws_inspector_rules_packages" "rules" {
   provider = "aws.master"
 }
 
-resource "aws_inspector_resource_group" "all" {
-  provider = "aws.master"
-  tags = {}
-}
-
 resource "aws_inspector_assessment_target" "default" {
   provider = "aws.master"
   name = "default-target"
-  resource_group_arn = "${aws_inspector_resource_group.all.arn}"
 }
 
 resource "aws_inspector_assessment_template" "default" {
--- org-member/inspector.tf
+++ org-member/inspector.tf
@@ -2,15 +2,9 @@ data "aws_inspector_rules_packages" "rules" {
   provider = "aws.member"
 }
 
-resource "aws_inspector_resource_group" "all" {
-  provider = "aws.member"
-  tags = {}
-}
-
 resource "aws_inspector_assessment_target" "default" {
   provider = "aws.member"
   name = "default-target"
-  resource_group_arn = "${aws_inspector_resource_group.all.arn}"
 }
 
 resource "aws_inspector_assessment_template" "default" {
