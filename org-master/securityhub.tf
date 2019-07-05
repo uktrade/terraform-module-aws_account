3de133d4a83cd4494a865b8ba4f62894f86273f0
Bowen Sun <bowensun@gmail.com>
2019-07-05 13:26:20 +0100

    GuardDuty, Macie and Inspector enabled by default in SecurityHub

--- org-master/securityhub.tf
+++ org-master/securityhub.tf
@@ -7,21 +7,3 @@ resource "aws_securityhub_standards_subscription" "master" {
   depends_on = ["aws_securityhub_account.master"]
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
 }
-
-resource "aws_securityhub_product_subscription" "securityhub-guardduty" {
-  provider = "aws.master"
-  depends_on = ["aws_securityhub_account.master"]
-  product_arn = "arn:aws:securityhub:${data.aws_region.master.name}::product/aws/guardduty"
-}
-
-resource "aws_securityhub_product_subscription" "securityhub-inspector" {
-  provider = "aws.master"
-  depends_on = ["aws_securityhub_account.master"]
-  product_arn = "arn:aws:securityhub:${data.aws_region.master.name}::product/aws/inspector"
-}
-
-resource "aws_securityhub_product_subscription" "securityhub-macie" {
-  provider = "aws.master"
-  depends_on = ["aws_securityhub_account.master"]
-  product_arn = "arn:aws:securityhub:${data.aws_region.master.name}::product/aws/macie"
-}
