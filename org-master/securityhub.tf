f002545bd93505982270294772fe2f769978b02c
Bowen Sun <bowensun@gmail.com>
2020-07-30 14:33:39 +0100

    fix arn paths

--- org-master/securityhub.tf
+++ org-master/securityhub.tf
@@ -10,12 +10,12 @@ resource "aws_securityhub_standards_subscription" "master-cis" {
 
 resource "aws_securityhub_standards_subscription" "master-aws" {
   provider = aws.master
-  standards_arn = "arn:aws:securityhub:::standards/aws-foundational-security-best-practices/v/1.0.0"
+  standards_arn = "arn:aws:securityhub:${data.aws_region.master.name}::standards/aws-foundational-security-best-practices/v/1.0.0"
   depends_on = [aws_securityhub_account.master]
 }
 
 resource "aws_securityhub_standards_subscription" "master-pci" {
   provider = aws.master
-  standards_arn = "arn:aws:securityhub:::standards/pci-dss/v/3.2.1"
+  standards_arn = "arn:aws:securityhub:${data.aws_region.master.name}::standards/pci-dss/v/3.2.1"
   depends_on = [aws_securityhub_account.master]
 }
--- org-member/securityhub.tf
+++ org-member/securityhub.tf
@@ -10,12 +10,12 @@ resource "aws_securityhub_standards_subscription" "member-cis" {
 
 resource "aws_securityhub_standards_subscription" "member-aws" {
   provider = aws.member
-  standards_arn = "arn:aws:securityhub:::standards/aws-foundational-security-best-practices/v/1.0.0"
+  standards_arn = "arn:aws:securityhub:${data.aws_region.master.name}::standards/aws-foundational-security-best-practices/v/1.0.0"
   depends_on = [aws_securityhub_account.member]
 }
 
 resource "aws_securityhub_standards_subscription" "member-pci" {
   provider = aws.member
-  standards_arn = "arn:aws:securityhub:::standards/pci-dss/v/3.2.1"
+  standards_arn = "arn:aws:securityhub:${data.aws_region.master.name}::standards/pci-dss/v/3.2.1"
   depends_on = [aws_securityhub_account.member]
 }
