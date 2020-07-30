0a7771f35748d991c7b99c40720fdbed276ff592
Bowen Sun <bowensun@gmail.com>
2020-07-30 14:06:01 +0100

    enable more security standard checks in secutityhub

--- org-master/securityhub.tf
+++ org-master/securityhub.tf
@@ -2,8 +2,20 @@ resource "aws_securityhub_account" "master" {
   provider = aws.master
 }
 
-resource "aws_securityhub_standards_subscription" "master" {
+resource "aws_securityhub_standards_subscription" "master-cis" {
   provider = aws.master
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+  depends_on = [aws_securityhub_account.master]
+}
+
+resource "aws_securityhub_standards_subscription" "master-aws" {
+  provider = aws.master
+  standards_arn = "arn:aws:securityhub:::standards/aws-foundational-security-best-practices/v/1.0.0"
+  depends_on = [aws_securityhub_account.master]
+}
+
+resource "aws_securityhub_standards_subscription" "master-pci" {
+  provider = aws.master
+  standards_arn = "arn:aws:securityhub:::standards/pci-dss/v/3.2.1"
   depends_on = [aws_securityhub_account.master]
 }
--- org-member/securityhub.tf
+++ org-member/securityhub.tf
@@ -2,8 +2,20 @@ resource "aws_securityhub_account" "member" {
   provider = aws.member
 }
 
-resource "aws_securityhub_standards_subscription" "member" {
+resource "aws_securityhub_standards_subscription" "member-cis" {
   provider = aws.member
   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+  depends_on = [aws_securityhub_account.member]
+}
+
+resource "aws_securityhub_standards_subscription" "member-aws" {
+  provider = aws.member
+  standards_arn = "arn:aws:securityhub:::standards/aws-foundational-security-best-practices/v/1.0.0"
+  depends_on = [aws_securityhub_account.member]
+}
+
+resource "aws_securityhub_standards_subscription" "member-pci" {
+  provider = aws.member
+  standards_arn = "arn:aws:securityhub:::standards/pci-dss/v/3.2.1"
   depends_on = [aws_securityhub_account.member]
 }
