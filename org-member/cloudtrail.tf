59dbcfafb532c54785ef631e2cb8896976614fe1
Bowen Sun <bowensun@gmail.com>
2020-08-12 11:28:36 +0100

    fix issue introduced in AWS provider 3.0

--- org-member/cloudtrail.tf
+++ org-member/cloudtrail.tf
@@ -6,7 +6,7 @@ resource "aws_cloudtrail" "trail" {
   enable_log_file_validation = true
   kms_key_id = aws_kms_key.cloudtrail-kms.arn
   s3_bucket_name = aws_s3_bucket.cloudtrail-s3.id
-  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail.arn
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail.arn}:*"
   cloud_watch_logs_role_arn = aws_iam_role.cloudtrail_log.arn
   event_selector {
     read_write_type = "All"
