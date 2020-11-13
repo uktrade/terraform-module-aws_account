0c344972d3fbe3367b0b461a402bc18bf99550df
Bowen Sun <bowensungmail.com>
2020-11-13 12:17:52 +0000

    disable Macie classic until terraform supports new Macie

--- org-member/macie.tf
+++ org-member/macie.tf
@@ -1,4 +1,7 @@
-resource "aws_macie_member_account_association" "member" {
-  provider = aws.master
-  member_account_id = data.aws_caller_identity.member.account_id
-}
+#
+# Macie classic should not be used.
+#
+# resource "aws_macie_member_account_association" "member" {
+#   provider = aws.master
+#   member_account_id = data.aws_caller_identity.member.account_id
+# }
