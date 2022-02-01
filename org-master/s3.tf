6f321c5d20b886b579fdb2f49fa3e5702452fc16
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2022-02-01 15:34:25 +0000

    Add  Sentinel central S3 bucket

---
+++ org-master/s3.tf
@@ -0,0 +1,100 @@
+resource "aws_s3_bucket" "sentinel_logs" {
+  provider = aws.master
+  bucket   = "sentinel-logs-${data.aws_caller_identity.master.account_id}"
+  acl      = "private"
+  tags     = {
+    Operator = "Microsoft_Sentinel_Automation_Script"
+  }
+}
+
+resource "aws_s3_bucket_policy" "sentinel_logs" {
+  provider = aws.master
+  bucket   = aws_s3_bucket.sentinel_logs.id
+  policy   = data.aws_iam_policy_document.sentinel_logs.json
+}
+
+data "aws_iam_policy_document" "sentinel_logs" {
+  provider = aws.master
+
+  statement {
+    sid = "Allow Sentinel role read access to S3 log bucket"
+    actions = ["s3:Get*","s3:List*"]
+    effect   = "Allow"
+    resources = [aws_s3_bucket.sentinel_logs.arn]
+    principals {
+      type = "AWS"
+      identifiers = [aws_iam_role.sentinel_role.arn]
+    }
+  }
+
+  statement {
+    sid = "Allow GuardDuty to use the getBucketLocation operation"
+    actions = ["s3:GetBucketLocation"]
+    effect = "Allow"
+    resources = [aws_s3_bucket.sentinel_logs.arn]
+    principals {
+        type = "Service"
+        identifiers = ["guardduty.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid = "Allow GuardDuty to upload objects to the bucket"
+    actions = ["s3:PutObject"]
+    effect = "Allow"
+    resources = ["${aws_s3_bucket.sentinel_logs.arn}/*"]
+    principals {
+        type = "Service"
+        identifiers = ["guardduty.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid = "Deny GuardDuty unencrypted object uploads."
+    actions = ["s3:PutObject"]
+    effect = "Deny"
+    resources = ["${aws_s3_bucket.sentinel_logs.arn}/*"]
+    principals {
+        type = "Service"
+        identifiers = ["guardduty.amazonaws.com"]
+    }
+    condition {
+      test = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values = ["aws:kms"]
+    }
+  }
+
+  statement {
+    sid = "Deny GuardDuty incorrect encryption header."
+    actions = ["s3:PutObject"]
+    effect = "Deny"
+    resources = ["${aws_s3_bucket.sentinel_logs.arn}/*"]
+    principals {
+        type = "Service"
+        identifiers = ["guardduty.amazonaws.com"]
+    }
+    condition {
+      test = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption-aws-kms-key-id"
+      values = [aws_kms_key.sentinel_guard_duty.arn]
+    }
+  }
+
+  statement {
+    sid = "Deny non-HTTPS access."
+    actions = ["s3:*"]
+    effect = "Deny"
+    resources = ["${aws_s3_bucket.sentinel_logs.arn}/*"]
+    principals {
+        type = "Service"
+        identifiers = ["guardduty.amazonaws.com"]
+    }
+    condition {
+      test = "Bool"
+      variable = "aws:SecureTransport"
+      values = ["false"]
+    }
+  }
+  
+}
