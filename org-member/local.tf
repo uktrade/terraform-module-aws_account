2301271af758dea782ef50ee10bbbee207f21628
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2022-02-04 15:29:34 +0000

    Add Sentinel tag local values for members.

---
+++ org-member/local.tf
@@ -0,0 +1,6 @@
+# Sentinel Local Values
+locals {
+  sentinel_common_resource_tag_name = "Operator"
+  sentinel_common_resource_tag_value = "Microsoft_Sentinel_Automation_Script"
+  sentinel_common_resource_tag = {"${local.sentinel_common_resource_tag_name}" = "${local.sentinel_common_resource_tag_value}"}
+}
