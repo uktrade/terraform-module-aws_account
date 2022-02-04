4d7a0da5d2ad97ebcc9f35aa2292041bda53c16a
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2022-02-04 13:06:50 +0000

    Add CloudTrail SQS queue and policy.

--- org-master/sqs.tf
+++ org-master/sqs.tf
@@ -12,6 +12,11 @@ resource "aws_sqs_queue" "sentinel_guardduty_queue" {
   tags     = {
     Operator = "Microsoft_Sentinel_Automation_Script"
   }
+
+resource "aws_sqs_queue" "sentinel_cloudtrail_queue" {
+  provider = aws.master
+  name     = "microsoft-sentinel-s3-cloudtrail"
+  tags     = tomap(local.sentinel_common_resource_tag)
 }
 
 resource "aws_sqs_queue_policy" "sentinel_flowlog_queue" {
@@ -32,6 +37,18 @@ resource "aws_sqs_queue_policy" "sentinel_guardduty_queue" {
   policy    = templatefile("${path.module}/policies/s3-sqs.json",
     {
       aws_sqs_queue_arn = aws_sqs_queue.sentinel_guardduty_queue.arn
+      aws_s3_bucket_arn = aws_s3_bucket.sentinel_logs.arn
+      aws_iam_role_arn = aws_iam_role.sentinel_role.arn
+    }
+  )
+}
+
+resource "aws_sqs_queue_policy" "sentinel_cloudtrail_queue" {
+  provider  = aws.master
+  queue_url = aws_sqs_queue.sentinel_cloudtrail_queue.id
+  policy    = templatefile("${path.module}/policies/s3-sqs.json",
+    {
+      aws_sqs_queue_arn = aws_sqs_queue.sentinel_cloudtrail_queue.arn
       aws_s3_bucket_arn = aws_s3_bucket.sentinel_logs.arn
       aws_iam_role_arn = aws_iam_role.sentinel_role.arn
     }
