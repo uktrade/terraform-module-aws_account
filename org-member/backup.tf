c0127923a3b14e67605b19641a6615f0fbbcc121
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2022-03-29 16:44:43 +0100

    Create backup policy member account attachment.

--- org-member/backup.tf
+++ org-member/backup.tf
@@ -8,3 +8,9 @@ resource "aws_backup_vault" "daily8_weekly5_monthly14" {
   name = "daily8-weekly5-monthly14"
   kms_key_arn = data.aws_kms_key.aws_backup.arn
 }
+
+resource "aws_organizations_policy_attachment" "backup_daily8_weekly5_monthly14" {
+  provider = aws.master
+  policy_id = var.org["organization_policy_backup_d8w5m14"]
+  target_id = data.aws_caller_identity.member.account_id
+}
