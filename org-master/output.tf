059da7c35a9ec8f5dc80826807f99e48e2363da2
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2022-03-29 16:42:33 +0100

    Add "d8-w5-m14" backup policy and output id.

--- org-master/output.tf
+++ org-master/output.tf
@@ -5,6 +5,7 @@ output "org_master" {
             "aws_profile" = var.org["aws_profile"],
             "organization_arn" = aws_organizations_organization.org.arn,
             "organization_id" = aws_organizations_organization.org.id,
+            "organization_policy_backup_d8w5m14" = aws_organizations_policy.backup_daily8_weekly5_monthly14.id,
             "cloudtrail_arn" = aws_cloudtrail.trail.arn,
             "cloudwatch_eventbus_arn" = "arn:aws:events:${data.aws_region.master.name}:${data.aws_caller_identity.master.account_id}:event-bus/default",
             "config_id" = aws_config_configuration_recorder.master_config.id,
