705a2cc1b9c2c78e77b6cfd3d4e93e21f3b08245
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2022-05-09 16:21:55 +0100

    Add aws-backup member module resources.

--- org-member/backup.tf
+++ org-member/backup.tf
@@ -1,3 +1,5 @@
+# AWS Backup
+
 data "aws_kms_key" "aws_backup" {
   provider = aws.member
   key_id = "alias/aws/backup"
@@ -13,4 +15,73 @@ resource "aws_organizations_policy_attachment" "backup_daily8_weekly5_monthly14"
   provider = aws.master
   policy_id = var.org["organization_policy_backup_d8w5m14"]
   target_id = data.aws_caller_identity.member.account_id
+}
+
+# Alerting
+
+resource "aws_backup_vault_notifications" "daily8_weekly5_monthly14" {
+  provider = aws.member
+  backup_vault_name = aws_backup_vault.daily8_weekly5_monthly14.name
+  sns_topic_arn = aws_sns_topic.org_backup_sns.arn
+  backup_vault_events = ["BACKUP_JOB_COMPLETED"]
+}
+
+## SNS
+
+resource "aws_sns_topic" "org_backup_sns" {
+  provider = aws.member
+  name = "org-backup-sns"
+}
+
+resource "aws_sns_topic_policy" "org_backup_sns" {
+  provider = aws.member
+  arn = aws_sns_topic.org_backup_sns.id
+  policy = data.aws_iam_policy_document.org_backup_sns.json
+}
+
+resource "aws_sns_topic_subscription" "org_backup_sns_to_lambda" {
+  provider = aws.master
+  topic_arn = aws_sns_topic.org_backup_sns.arn
+  protocol = "lambda"
+  endpoint = var.org["config_to_slack_lambda_arn"]
+  depends_on = [
+    aws_sns_topic_policy.org_backup_sns
+  ]
+}
+
+## Lambda
+
+resource "aws_lambda_permission" "aws_lambda_from_member_sns" {
+  provider = aws.master
+  statement_id = "AllowExecutionFromMember_${data.aws_caller_identity.member.account_id}"
+  principal = "sns.amazonaws.com"
+  action = "lambda:InvokeFunction"
+  function_name = var.org["config_to_slack_lambda_arn"]
+  source_arn = aws_sns_topic.org_backup_sns.arn
+}
+
+## IAM
+
+data "aws_iam_policy_document" "org_backup_sns" {
+  provider = aws.member
+  statement {
+    sid = "org_backup_sns"
+    effect = "Allow"
+    actions = ["SNS:Publish"]
+    principals {
+      type = "Service"
+      identifiers = ["backup.amazonaws.com"]
+    }
+    resources = [aws_sns_topic.org_backup_sns.id]
+  }
+  statement {
+    sid = "AllowSubscriptionFromMasterAccount"
+    effect = "Allow"
+    actions = ["SNS:Receive","SNS:Subscribe"]
+    principals {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.master.account_id}:root"]
+    }
+    resources = [aws_sns_topic.org_backup_sns.id]
+  }
 }
