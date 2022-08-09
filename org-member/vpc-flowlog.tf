7627a2f2e913b5edba3a14d494b853c61d784f37
shehzadashiq <shehzadashiq@hotmail.com>
2022-08-09 16:06:38 +0100

    Adding V5 Sentinel Logs

--- org-member/vpc-flowlog.tf
+++ org-member/vpc-flowlog.tf
@@ -36,6 +36,7 @@ resource "aws_flow_log" "sentinel_vpc_log" {
   for_each = data.aws_vpcs.vpcs.ids
   log_destination_type = "s3"
   log_destination = var.org["sentinel_vpc_s3_bucket"]
+  log_format = var.soc_config["sentinel_vpc_log_format"]
   vpc_id = each.key
   traffic_type = "ALL"
   tags = merge(
