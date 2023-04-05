e2c37c8b838270111703dfb714bb8a2c76707971
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2023-04-05 17:40:38 +0100

    Create AWSControlTowerExecution IAM role.

--- org-member/iam.tf
+++ org-member/iam.tf
@@ -410,3 +410,27 @@ data "aws_iam_policy_document" "default_dev_policy" {
     }
   }
 }
+
+resource "aws_iam_role" "control_tower_execution" {
+  provider = aws.member
+  name = "AWSControlTowerExecution"
+  assume_role_policy = data.aws_iam_policy_document.control_tower_execution.json
+}
+
+data "aws_iam_policy_document" "control_tower_execution" {
+  provider = aws.member
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.master.account_id}:root"]
+    }
+  }  
+}
+
+resource "aws_iam_role_policy_attachment" "control_tower_execution_admin_access" {
+  provider = aws.member
+  role = aws_iam_role.control_tower_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
