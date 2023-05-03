26d0d76c46314910d8d07066145d39c29417367a
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2023-05-03 11:52:38 +0100

    Add and use aws_organization_id local variable.

--- org-member/local.tf
+++ org-member/local.tf
@@ -3,4 +3,5 @@ locals {
   sentinel_common_resource_tag_name = "Operator"
   sentinel_common_resource_tag_value = "Microsoft_Sentinel_Automation_Script"
   sentinel_common_resource_tag = {"${local.sentinel_common_resource_tag_name}" = "${local.sentinel_common_resource_tag_value}"}
+  aws_organization_id = element(split("/", var.org["organization_arn"]), 1)
 }
