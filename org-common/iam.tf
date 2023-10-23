77d14bcece35aa6ab02e0d76a202e11a09fb6a82
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2023-10-23 14:02:06 +0100

    Create IAM policy for delegated user management.

--- org-common/iam.tf
+++ org-common/iam.tf
@@ -23,3 +23,57 @@ resource "aws_iam_role_policy_attachment" "control_tower_execution_admin_access"
   role = aws_iam_role.control_tower_execution.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
+
+# Delegated IAM User Manager Policy
+
+resource "aws_iam_policy" "iam_user_manager" {
+  provider = aws.common
+  name = "dit-iam-manager"
+  description = "Policy for IAM delegated manager access."
+  policy = data.aws_iam_policy_document.iam_user_manager.json
+}
+
+data "aws_iam_policy_document" "iam_user_manager" {
+  provider = aws.common
+  statement {
+    effect = "Allow"
+    actions = [
+        "iam:ListAccountAliases",
+				"iam:ListUsers",
+				"iam:ListGroupsForUser",
+        "iam:ListMFADevices",
+				"iam:ListAttachedUserPolicies",
+				"iam:ListUserTags",
+        "iam:ListAccessKeys",
+        "iam:ListPolicies",
+        "iam:ListServiceSpecificCredentials",
+        "iam:ListUserPolicies",
+        "iam:ListSigningCertificates",
+        "iam:ListSSHPublicKeys",
+        "iam:GetAccountSummary",
+				"iam:GetUser",
+				"iam:GetUserPolicy",
+        "iam:GetLoginProfile",
+				"iam:DeleteUser",
+        "iam:DeleteSSHPublicKey",
+        "iam:DeleteSigningCertificate",
+        "iam:DeleteServiceSpecificCredential",
+				"sso-directory:ListMfaDevicesForUser",
+				"sso-directory:ListGroupsForUser",
+				"sso-directory:SearchUsers",
+				"sso-directory:DescribeUsers",
+        "sso-directory:DescribeDirectory",
+				"sso-directory:DescribeUserByUniqueAttribute",
+				"sso-directory:DescribeUser",
+				"sso-directory:DisableUser",
+				"sso-directory:DeleteUser",
+        "sso-directory:DeleteMfaDeviceForUser",
+        "sso:ListInstances",
+        "sso:ListDirectoryAssociations",
+        "sso:GetSsoConfiguration",
+        "sso:GetSSOStatus",
+        "sso:DisassociateProfile"
+			]
+    resources = ["*"]
+  }  
+}
