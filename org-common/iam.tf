1cc7134c72e941c17dbaa49a5a8da2f83484dde6
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2023-11-20 11:55:31 +0000

    Add additional required notification action.

--- org-common/iam.tf
+++ org-common/iam.tf
@@ -84,6 +84,7 @@ data "aws_iam_policy_document" "iam_user_manager" {
       "sso:DisassociateProfile",
       "sso:DescribeRegisteredRegions",
       "sso:GetMfaDeviceManagementForDirectory",
+      "notifications:ListNotificationHubs"
       ]
     resources = ["*"]
   }
