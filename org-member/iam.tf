673daa95a0b85cac02925b1d17ef193cfc41261e
Robert McHugh <robert@madetech.com>
2023-12-15 16:13:07 +0000

    Add inital default iam password policy

--- org-member/iam.tf
+++ org-member/iam.tf
@@ -429,3 +429,13 @@ data "aws_iam_policy_document" "default_dev_policy" {
     }
   }
 }
+
+resource "aws_iam_account_password_policy" "strict" {
+  provider = aws.member
+  minimum_password_length        = 8
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+}
