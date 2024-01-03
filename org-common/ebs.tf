d85c769ce5d49a9d6497e621a83f3a6a1404e614
Robert McHugh <robert@madetech.com>
2024-01-03 15:03:33 +0000

    Update resource name to use underscores

--- org-common/ebs.tf
+++ org-common/ebs.tf
@@ -1,4 +1,4 @@
-resource "aws_ebs_encryption_by_default" "ebs-default-encryption" {
+resource "aws_ebs_encryption_by_default" "ebs_default_encryption" {
     provider = aws.common
     enabled = true
 }
