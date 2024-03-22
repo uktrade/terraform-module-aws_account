e614b590704665b43ad85a25ae7690c4e4b5b2c4
Shehzad Ali <shehzadashiq@hotmail.com>
2024-03-22 15:00:31 +0000

    Ignore Interpolation issues

--- org-member/local.tf
+++ org-member/local.tf
@@ -2,6 +2,7 @@
 locals {
   sentinel_common_resource_tag_name  = "Operator"
   sentinel_common_resource_tag_value = "Microsoft_Sentinel_Automation_Script"
-  sentinel_common_resource_tag       = { local.sentinel_common_resource_tag_name = local.sentinel_common_resource_tag_value }
+  # tflint-ignore: terraform_deprecated_interpolation
+  sentinel_common_resource_tag       = { "${local.sentinel_common_resource_tag_name}" = "${local.sentinel_common_resource_tag_value}" }
   aws_organization_id                = element(split("/", var.org["organization_arn"]), 1)
 }
