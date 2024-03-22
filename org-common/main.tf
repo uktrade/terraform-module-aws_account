a59b04f64320e7577046ae8991c35c871aa97311
Shehzad Ali <shehzadashiq@hotmail.com>
2024-03-22 12:23:07 +0000

    Apply checks to org-common

--- org-common/main.tf
+++ org-common/main.tf
@@ -1,46 +1,4 @@
-variable "org" {
-  type = any
-}
-
-variable "password_policy_minimum_password_length" {
-  default = 8
-}
-
-variable "password_policy_require_lowercase_characters" {
-  default = true
-}
-
-variable "password_policy_require_numbers" {
-  default = true
-}
-
-variable "password_policy_require_uppercase_characters" {
-  default = true
-}
-variable "password_policy_require_symbols" {
-  default = true
-}
-variable "password_policy_allow_users_to_change_password" {
-  default = true
-}
-
-variable "password_policy_password_reuse_prevention" {
-  default = 24
-}
-
-variable "password_policy_max_password_age" {
-  default = 0
-}
-
-terraform {
-  required_providers {
-    aws = {
-      source                = "hashicorp/aws"
-      configuration_aliases = [aws.common]
-    }
-  }
-}
-
+# tflint-ignore: terraform_unused_declarations
 data "aws_region" "common" {
   provider = aws.common
 }
