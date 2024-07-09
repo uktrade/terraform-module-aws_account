e4b851aea468eaadc62338d6eb7f55901572bf1d
Aadam Ali <57071686+Aadam-Ali@users.noreply.github.com>
2024-07-09 09:52:27 +0100

    Add 'copilot-application' tag for CodeStar connection
    This tag seems to be present for the CodeStar connections that I have
    been importing, let's add it into the Terraform so that the connections
    don't lose their tags.

--- org-common/codestar.tf
+++ org-common/codestar.tf
@@ -2,4 +2,7 @@ resource "aws_codestarconnections_connection" "github" {
   provider      = aws.common
   name          = var.codestar_connection_name
   provider_type = "GitHub"
+  tags          = {
+    copilot-application = var.codestar_connection_name
+  }
 }
