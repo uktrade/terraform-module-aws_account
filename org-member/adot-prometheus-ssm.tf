276ca35ec464f786d831875a939f369fcf406bcf
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2024-07-19 12:48:58 +0100

    Add SSM parameters with default config for the ADOT ECS sidecar image
    This enables services running in ECS/DBT Platform to configure the ADOT (AWS Distro for Open Telemetry) sidecar image to ship local container metrics into the dev and prod promeheus workspaces in the `sre-tools` AWS account.
    
    Example usage from a copilot service manifest.yml:
    
    ```
    sidecars:
      otel_sidecar:
        image: 'public.ecr.aws/aws-observability/aws-otel-collector:latest'
        secrets:
          AOT_CONFIG_CONTENT: /observability/prometheus/adot_config
    ```

---
+++ org-member/adot-prometheus-ssm.tf
@@ -0,0 +1,35 @@
+locals {
+  tags = {
+    managed-by = "DBT Platform - Terraform"
+  }
+
+  prod = {
+    assume_role = "arn:aws:iam::480224066791:role/amp-prometheus-role"
+    endpoint = "https://aps-workspaces.eu-west-2.amazonaws.com/workspaces/ws-7297af06-7c1a-4bfc-affd-4abe053797e16e/api/v1/remote_write"
+  }
+
+  dev = {
+    assume_role = "arn:aws:iam::480224066791:role/amp-prometheus-dev-role"
+    endpoint = "https://aps-workspaces.eu-west-2.amazonaws.com/workspaces/ws-d9fd4d97-49cc-4b89-9f4c-025e275704eec6/api/v1/remote_write"
+  }
+}
+
+resource "aws_ssm_parameter" "adot-prometheus-dev-config" {
+  name            = "/observability/prometheus-dev/adot_config"
+  description     = "Configuration to enable the ADOT sidecar image to ship an ECS service contaniers metrics into Prometheus."
+  type            = "String"
+  insecure_value  = templatefile("${path.module}/adot-prometheus-config.yaml.tmpl", local.dev)
+  tier            = "Intelligent-Tiering"
+
+  tags = local.tags
+}
+
+resource "aws_ssm_parameter" "adot-prometheus-config" {
+  name            = "/observability/prometheus/adot_config"
+  description     = "Configuration to enable the ADOT sidecar image to ship an ECS service contaniers metrics into Prometheus."
+  type            = "String"
+  insecure_value  = templatefile("${path.module}/adot-prometheus-config.yaml.tmpl", local.prod)
+  tier            = "Intelligent-Tiering"
+
+  tags = local.tags
+}
