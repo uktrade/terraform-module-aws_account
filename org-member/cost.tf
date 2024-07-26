08c412aebfe30b4b26cc04cd39d33dad0ec824e9
Shehzad Ali <shehzadashiq@hotmail.com>
2024-07-26 13:32:00 +0100

    Enable Cost Optimisation Policy on member account

--- org-member/cost.tf
+++ org-member/cost.tf
@@ -1,6 +1,6 @@
-# resource "aws_iam_role" "cost_optimisation" {
-#   provider           = aws.member
-#   name               = "dit-cost-optimisation"
-#   description        = "Role used by AWS provides the permissions necessary for a member account to have full access to Cost Optimization Hub."
-#   assume_role_policy = file("${path.module}/policies/org-policy-cost-member.json")
-# }
+resource "aws_iam_policy" "cost_optimisation" {
+  provider           = aws.member
+  name               = "dit-cost-optimisation"
+  description        = "Role used by AWS provides the permissions necessary for a member account to have full access to Cost Optimization Hub."
+  policy = file("${path.module}/policies/org-policy-cost-member.json")
+}
