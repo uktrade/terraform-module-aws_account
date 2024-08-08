985d89f9dbc337b6ec9a9b1bf9f3226f2fb86551
James Moore <james.moore@digital.trade.gov.uk>
2024-08-08 17:46:34 +0100

    add parent_id field to allow terraform to assign an account to an OU

--- new-org-member/org.tf
+++ new-org-member/org.tf
@@ -1,7 +1,18 @@
-# Add account to AWS Organization
+# Add account to AWS Organization and assign to an OU
+data "aws_organizations_organization" "org" {
+  provider = aws.master
+}
+
+data "aws_organizations_organizational_unit" "ou" {
+  provider = aws.master
+  parent_id = data.aws_organizations_organization.org.roots[0].id
+  name      = var.member["org_ou"]
+}
+
 resource "aws_organizations_account" "member" {
   provider                   = aws.master
   name                       = var.member["name"]
   email                      = var.member["email"]
   iam_user_access_to_billing = "ALLOW"
+  parent_id = data.aws_organizations_organizational_unit.ou.id
 }
