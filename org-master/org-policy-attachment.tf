7114b4bb4100c3bf2759f13aa1f1c8be77354d02
James Moore <james.moore@digital.trade.gov.uk>
2025-03-21 10:36:36 +0000

    Restrict SSE-C uploads to S3 at Root OU layer from now on

--- org-master/org-policy-attachment.tf
+++ org-master/org-policy-attachment.tf
@@ -1,10 +1,7 @@
 # Organisation Policy attachments 
 
 ## Restrict SSE-C Uploads to S3. 
-### At time of writing (26th Feb 2025), we only wanted to enable this on non prod OUs
-### Once people are happy, we want to enable this on the root OU instead
 resource "aws_organizations_policy_attachment" "restrictSSECUploads" {
-  for_each = toset(var.rcp_accounts)
   policy_id = aws_organizations_policy.restrictSSECUploads.id
-  target_id = aws_organizations_organizational_unit.org_ou_structure[each.key].id
+  target_id = aws_organizations_organization.org.roots[0].id
 }
